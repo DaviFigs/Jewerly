b02007d86fbf642eaaa844d5a65cbda4ebaad261
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-21 14:24:59 -0400

    Auth file creation

---
+++ auth/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render, redirect
+
+def login(request):
+    return render(request,)
+
--- jew_core/settings.py
+++ jew_core/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jew_core',
+    'auth',
 ]
 
 MIDDLEWARE = [
