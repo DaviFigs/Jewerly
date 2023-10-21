22f3c48ceb595006f0b18ce983bf042f2ce7829b
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-21 18:32:09 -0400

    First urls

--- jew_core/settings.py
+++ jew_core/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'users',
     'products',
     'orders',
+    'base'
 
 ]
 
--- jew_core/urls.py
+++ jew_core/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from base import views
 
 urlpatterns = [
+    path('',views.home),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
-    path('orders/', include('orders.urls'))
+    path('orders/', include('orders.urls')),
+    path('base/', include ('base.urls')),
 ]
