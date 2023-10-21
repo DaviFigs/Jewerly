a2d6da0609374e20ae58778ec87912c1577cb9ad
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-21 14:55:25 -0400

    Bug Fix

--- auth/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render, redirect
-
-def login(request):
-    return render(request,)
-
--- jew_core/settings.py
+++ jew_core/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'jew_core',
-    'auth',
+    'users',
 ]
 
 MIDDLEWARE = [
