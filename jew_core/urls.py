483cf1f2b70cb53b187234fefe0ee4ac0422eb49
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-22 18:30:14 -0400

    Templates creation

--- jew_core/urls.py
+++ jew_core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from base import views
+from main_view import views
 
 urlpatterns = [
     path('',views.home),
--- main_view/views.py
+++ main_view/views.py
@@ -1,3 +1,3 @@
 from django.shortcuts import render
-
-# Create your views here.
+def home(request):
+    return render(request, 'site.html')
