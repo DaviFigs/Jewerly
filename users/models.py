ffb261a3273f9abc1338c4dcefdf1d7122959975
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-22 12:58:40 -0400

    Fixes

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class MyUser(AbstractUser):
-    cpf = models.CharField(max_length=11, default='00000000000', null=True)
+    cpf = models.CharField(max_length=11, blank=True, null=True)
     past_buys = models.IntegerField(default=0)
 
