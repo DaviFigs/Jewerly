272096dccde257c34cfe420e756dc3d5cbd54b25
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-10-24 01:24:28 -0400

    Users URLS 2.0

--- users/urls.py
+++ users/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
+    path('logout/', views.logout, name="logouts"),
     path('login/', views.render_login, name="render_login"),
     path('auth_login/', views.auth_login, name='auth_login'),
 
-    path('register/', views.render_register, name="register"),
+    path('register/', views.render_register, name="render_register"),
+    path('auth_register/', views.auth_register, name="auth_register"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,16 +2,22 @@ from django.shortcuts import render,redirect
 from .models import MyUser
 from django.contrib.messages import constants
 from django.contrib import messages
+from django.contrib.auth import authenticate, login as logon, logout as logouts
 from . forms import *
 
+def logout(request):
+    if request.user.is_authenticated:
+        logouts(request)
+        return redirect('main')
+    else:
+        messages.add_message(request, constants.WARNING,'Você precisa estar logado')
+        return redirect('main')
 def render_login(request):
     if request.user.is_authenticated:
         messages.add_message(request, constants.WARNING, 'Você já está logado!')
         return redirect('main')
     else:
-
         return render(request, 'login.html')
-
 def render_register(request):
     if request.user.is_authenticated:
         messages.add_message(request, constants.WARNING, 'Você já está logado!')
@@ -20,4 +26,23 @@ def render_register(request):
         return render(request,('signup.html'))
     
 def auth_login(request):
+    if request.method != 'POST':
+        messages.add_message(request, constants.WARNING, 'Método HTTP inválido')
+        return redirect('main')
+    else:
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(username = username, password = password)
+            if user == None:
+                messages.add_message(request, constants.WARNING, f'Login ou senha incorretos {user}')
+                return redirect('render_login')
+            elif user is not None:
+                logon(request, user)
+                messages.add_message(request, constants.SUCCESS, 'Login efetuado com sucesso')
+                return redirect('main')
+        except:
+            messages.add_message(request, constants.WARNING, 'Erro interno')
+            return redirect('main')
+def auth_register(request):
     pass
