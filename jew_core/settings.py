575e112df1b868ddc40836f64830561f86c19469
Davi <davi@davi.py>
2023-11-03 21:42:40 -0400

    Modifying the code for Linux

--- jew_core/settings.py
+++ jew_core/settings.py
@@ -102,8 +102,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT= os.path.join('static')
+if DEBUG:
+    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+else:
+    STATIC_ROOT= os.path.join('static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
