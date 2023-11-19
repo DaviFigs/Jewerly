e36667b3040799d445957ee6b68cfd5cfdac1630
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-11-18 22:39:06 -0400

    Post form for purchase

--- orders/views.py
+++ orders/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def finishing_purchase(request):
+    #getting the product buys
+    products = request.POST.getlist('check')
+    print(products)
     return render (request, 'finishing_purchase.html')
+
