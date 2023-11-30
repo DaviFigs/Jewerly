3866eab8c12cc7985a5b91414716b1d6a9a08da1
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-11-30 14:01:13 -0400

    IF on html

--- base/views.py
+++ base/views.py
@@ -5,10 +5,12 @@ def search(request):
     typed = request.GET.get('product_name')
     product_list = df.search_products(typed)
     print(product_list)
+    print(len(product_list))
+    if len(product_list) == 0:
+        product_list = 0
     context = {
         'search':typed,
         'products':product_list,
-        'count':df.count_products(product_list)
     }
     return render(request, 'search_list.html', context)
     
