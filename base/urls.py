ecf23e05f46dbd5b12990e0b4445acf5664350f1
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-03 23:36:26 -0400

    Making Filters on search

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search, name="search"),
+    path('search/<str:product_search>/', views.search, name="search_post"),    
     
 ]
--- base/views.py
+++ base/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from base import defs as df
-def search(request):
-    typed = request.GET.get('search_by')
-    product_list = df.search_products(typed)
 
-    if len(product_list) == 0:
-        product_list = 0
-    context = {
-        'search':typed,
-        'products':product_list,
-    }
-    return render(request, 'search_list.html', context)
+
+#What is working right now
+def search(request, product_name=''):
+    if request.method == 'GET':
+        user_search = request.GET.get('search_by')
+        product_list = df.search_products(user_search)
+
+        if len(product_list) == 0:
+            product_list = 0
+        context = {
+            'search':user_search,
+            'products':product_list,
+        }
+        return render(request, 'search_list.html', context)
     
+    elif request.method == 'POST':
+        user_search = product_name
+        search_filter = request.POST.get('filter')
+        
+        
+        
