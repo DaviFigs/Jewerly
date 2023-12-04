adbceb200d69681d9a34815065949e745ce1da4a
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-12-04 00:05:17 -0400

    Trying to make this shiiiiiit

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.search, name="search"),
-    path('search/<str:product_search>/', views.search, name="search_post"),    
     
 ]
--- base/views.py
+++ base/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render,redirect
 from base import defs as df
 
 
-#What is working right now
-def search(request, product_name=''):
+def search(request):
     if request.method == 'GET':
         user_search = request.GET.get('search_by')
         product_list = df.search_products(user_search)
@@ -16,9 +15,17 @@ def search(request, product_name=''):
         }
         return render(request, 'search_list.html', context)
     
-    elif request.method == 'POST':
-        user_search = product_name
-        search_filter = request.POST.get('filter')
+    elif request.method == 'POST':#adding filter on our code
         
+        user_search = request.POST.get('user_search')
+        search_filter = request.POST.get('search_filter')
+        
+        product_list = df.search_products(user_search)
+        context={
+            'filter':search_filter,
+            'search':user_search,
+            'products':product_list,
+        }
+        return render(request, 'search_list.html', context)
         
         
